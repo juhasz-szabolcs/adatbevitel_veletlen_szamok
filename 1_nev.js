379c9732e826d0e143716d0ac7e0be76a91d3875
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-01-29 10:43:46 +0100

    1-es nev, 2-es teglalap

---
+++ 1_nev.js
@@ -0,0 +1,4 @@
+// 1. Írj programot, ami beolvassa a felhasználó nevét, majd köszön neki!
+
+let felhasznalo_nev =prompt("Kérlek add meg a neved!", "pl. János")
+console.log(`Szia ${felhasznalo_nev}!`)
