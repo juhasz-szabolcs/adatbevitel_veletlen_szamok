a20d8c9e8a4cff9f87fec8ed9f4db3efaf533ca3
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2026-01-29 12:12:25 +0100

    -

---
+++ 6_inch_cm.js
@@ -0,0 +1,3 @@
+// 6. Írj programot, ami bekér egy inch értéket és átszámolja cm-be! 
+// (1 inch = 2,71 cm)
+
---
+++ 8_ora.js
@@ -0,0 +1,4 @@
+// 8. Írj programot, ami bekéri a dátumot (nap) és az órát, 
+// amiből kiszámolja, hogy a hónap 
+// hányadik órájában vagyunk!
+
